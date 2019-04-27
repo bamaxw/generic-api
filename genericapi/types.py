e519361ecf490c1be76f060c6b6095bf44311b2c
maximus <max.wasylow@trinitymirror.com>
2019-04-27 17:29:41 +0100

    i should probably finish for today

--- genericapi/types.py
+++ genericapi/types.py
@@ -1,4 +1,6 @@
-from typing import Any, Awaitable, Callable, Dict, Response
+from typing import Any, Awaitable, Callable, Dict
+
+from aiohttp.web import Response
 
 AsyncRouteHandler = Callable[..., Awaitable[Response]]
 RawCorsConfig = Dict[str, Dict[str, Any]]
