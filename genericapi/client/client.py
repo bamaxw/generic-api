5df3cc469e525aafbe236f031b58e1997babbf4f
maximus <max.wasylow@trinitymirror.com>
2019-06-10 15:09:35 +0100

    further simplifying genericapi client

--- genericapi/client/client.py
+++ genericapi/client/client.py
@@ -8,41 +8,10 @@ log = logging.getLogger(__name__)
 
 
 class Client(RequestEngine):
-    # Client allows some attributes to be set in a declarative way
-    # like so
-    # Client attributes
-    __slots__ = RequestEngine.__slots__ + ('service_name', 'prefix', 'host', 'env')
-    https: bool = False
+    __slots__ = RequestEngine.__slots__ + ('host',)
     host: Optional[str] = None
-    service_name: Optional[str] = None
-    prefix: str = ''
-
-    def __init__(self,
-                 env: Optional[str] = None,
-                 *,
-                 service_name: Optional[str] = None,
-                 prefix: str = '',
-                 host: Optional[str] = None,
-                 https: Optional[bool] = None) -> None:
-        if https is None:
-            https = self.https
-        if service_name is None:
-            service_name = self.service_name
-        if host is None:
-            host = self.host
-        if host is None:
-            if service_name is None or env is None:
-                raise TypeError("a client with not host must have both 'service_name' and 'env'")
-            if https:
-                host = 'https://'
-            else:
-                host = 'http://'
-            host += f'{service_name}-{env}.inyourarea.co.uk'
-        if prefix is None:
-            prefix = self.prefix
-        baseurl = host + prefix
-        super().__init__(baseurl)
-        self.host = host
-        self.service_name = service_name
-        self.prefix = prefix
-        self.env = env
+    def __init__(self, host: Optional[str] = None) -> None:
+        host = host or self.host
+        if not host:
+            raise ValueError('no host specified')
+        super().__init__(host)
